560eb8992bc46ac219a7ba50c204f90ad44e7dbf
Aryan Karadia <aryan.karadia@ucalgary.ca>
2023-03-21 18:09:31 -0600

    save-note lambda

--- functions/save-note/main.py
+++ functions/save-note/main.py
@@ -8,9 +8,12 @@ def lambda_handler(event, context):
     body = json.load(event['body'])
     email = body['email']
     id = body['id']
+    noteBody = body['body']
+    title = body['title']
+    when = body['when']
     access_token = event['headers']['Authorization']
     try:
-        table.put_item(Item={'email': email, 'id': id, 'access_token': access_token})
+        table.put_item(Item={'email': email, 'id': id, 'access_token': access_token, 'body': noteBody, 'title': title, 'when': when})
         return {
             'statusCode': 200,
             'body': json.dumps({
