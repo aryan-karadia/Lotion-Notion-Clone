0b92234fac3d8288c68b71bed9f50df7f6e855a6
Aryan Karadia <aryan.karadia@ucalgary.ca>
2023-03-23 16:28:20 -0600

    login functionality

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import Notes from './pages/Notes';
 import Layout from './components/Layout';
 import Edit from './pages/Edit';
 import NoteView from './pages/NoteView';
+import { useState } from 'react';
+import Login from './pages/Login';
 
 function App() {
+
+  const [user, setUser] = useState("");
 
   return (
     <>
       <BrowserRouter>
           <Routes>
-            <Route element={<Layout />}>
+            {!user ? (<Route path="*" element={<Login />} />)
+            : (
+              <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/Notes"/>} />
               <Route path="Notes/:id/edit" element={<Edit />} />
               <Route path="/Notes" element={<Notes />} />
               <Route path="Notes/:id/edit/:id" element={<Navigate to="/Notes/:id" />} />
               <Route path="Notes/:id" element={<NoteView />} />
             </Route>
+            )
+          }
+
           </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1 style={{color: "white"}} >Login</h1>
+        </div>
+    )
+}
+
+export default Login
