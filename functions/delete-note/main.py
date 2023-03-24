d7586ff979649b95b54b448a125bbe665d3003ca
Aryan Karadia <aryan.karadia@ucalgary.ca>
2023-03-24 17:55:37 -0600

    google authentication

--- functions/delete-note/main.py
+++ functions/delete-note/main.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import requests
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('lotion-30148859')
@@ -10,6 +11,27 @@ def lambda_handler(event, context):
     access_token = event['headers']['access-token']
     id = body['id']
     if not access_token:
+        return {
+            'statusCode': 401,
+            'body': json.dumps({
+            'message': 'Unauthorized'
+            })
+        }
+    google_headers = {
+    "Authorization": f"Bearer {access_token}",
+    "Accept": "application/json",
+    }
+    response = requests.get("https://people.googleapis.com/v1/people/me?personFields=emailAddresses", headers=google_headers)
+    if response.status_code == 401:
+    # Return a 401 Unauthorized response if the access token is invalid
+        return {
+            "statusCode": 401,
+            "body": "Unauthorized",
+        }
+    googleData = json.loads(response.text)
+    print(googleData)
+    GoogleEmail = googleData["emailAddress"][0]["value"]
+    if GoogleEmail != body["email"]:
         return {
             'statusCode': 401,
             'body': json.dumps({
