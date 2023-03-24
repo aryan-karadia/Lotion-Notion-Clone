b0492759ecbe1543206768dd003debaee2a6c4e1
AdityaP16 <ap.prasad@outlook.com>
2023-03-23 20:47:33 -0600

    sign-in quick fix

--- src/App.js
+++ src/App.js
@@ -12,54 +12,47 @@ function App() {
   const [email, setEmail] = useState("");
   const [user, setUser] = useState([]);
   const [profile, setProfile] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(
-    () => {
-        if (user) {
-            axios
-                .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                    headers: {
-                        Authorization: `Bearer ${user.access_token}`,
-                        Accept: 'application/json'
-                    }
-                })
-                .then((res) => {
-                    setProfile(res.data);
-                    setEmail(res.data.email);
-                    console.log("email: " + email);
-                    console.log("user: " + user);
-                })
-                .catch((err) => console.log(err));
-        }
-    },
-    [ user ]
-);
-
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+            Accept: 'application/json'
+          }
+        })
+        .then((res) => {
+          setProfile(res.data);
+          setEmail(res.data.email);
+          setIsLoggedIn(true);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
 
   const responseMessage = (response) => {
-    console.log(response);
     setUser(response);
-    
   };
+
   const errorMessage = (error) => {
-      console.log(error);
+    console.log(error);
   };
 
   return (
     <>
-    {!user ? (        
-
-              <div>
-                <h2>React Google Login</h2>
-                <br />
-                <br />
-                <GoogleLogin onSuccess={responseMessage} onError={errorMessage}>Sign in to Lotion with Google</GoogleLogin>
-              </div>
-           )
-            : (
-      <BrowserRouter>
+      {!isLoggedIn ? (        
+        <div>
+          <h2>React Google Login</h2>
+          <br />
+          <br />
+          <GoogleLogin onSuccess={responseMessage} onError={errorMessage}>Sign in to Lotion with Google</GoogleLogin>
+        </div>
+      ) : (
+        <BrowserRouter>
           <Routes>
-              <Route element={<Layout />}>
+            <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/Notes"/>} />
               <Route path="Notes/:id/edit" element={<Edit />} />
               <Route path="/Notes" element={<Notes />} />
@@ -67,12 +60,10 @@ function App() {
               <Route path="Notes/:id" element={<NoteView />} />
             </Route>
           </Routes>
-      </BrowserRouter>
-            )
-          }
-
+        </BrowserRouter>
+      )}
     </>
-  )
+  );
 }
 
 export default App;
