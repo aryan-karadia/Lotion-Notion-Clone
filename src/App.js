01c6e6bf7e8205a7a2e42d3d4726333ed9f37f55
Aryan Karadia <karadiaaryan@gmail.com>
2023-12-20 19:34:14 -0700

    Fix Google OAuth URL and add missing closing tag for SVG element

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ function App() {
     if (user) {
       axios
         .get(
-          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          // `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&include_granted_scopes=true&
+          response_type=token&redirect_uri=https://lotion-notion-clone.netlify.app&client_id=228095394015-uat8jdgidrbpfgqem7rs0p92bq320sa3.apps.googleusercontent.com`,
           {
             headers: {
               Authorization: `Bearer ${user.access_token}`,
@@ -55,7 +57,6 @@ function App() {
     localStorage.removeItem("user");
     console.log("Logged out");
   };
-
 
   return (
     <>
@@ -110,7 +111,12 @@ function App() {
                   class="bi bi-google"
                   viewBox="0 0 16 16"
                 >
-                  <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+                  <path
+                    d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 
+                  15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 
+                  8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 
+                  3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"
+                  />
                 </svg>
               </button>
             </div>
