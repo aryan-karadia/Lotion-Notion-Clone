00954d00f3758d767ff4c9d1582c4d5c53eb9aac
Aryan Karadia <karadiaaryan@gmail.com>
2023-12-20 22:04:22 -0700

    Add Google OAuth 2.0 login functionality

--- src/App.js
+++ src/App.js
@@ -56,6 +56,43 @@ function App() {
     console.log("Logged out");
   };
 
+  /*
+   * Create form to request access token from Google's OAuth 2.0 server.
+   */
+  function oauthSignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+    var form = document.createElement("form");
+    form.setAttribute("method", "GET"); // Send as a GET request.
+    form.setAttribute("action", oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {
+      client_id:
+        "228095394015-jck5m1f8re24pkji0lsl493t7fg8ge6h.apps.googleusercontent.com",
+      redirect_uri: "https://lotionv2.netlify.app",
+      response_type: "token",
+      scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+      include_granted_scopes: "true",
+      state: "pass-through value",
+    };
+
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+      var input = document.createElement("input");
+      input.setAttribute("type", "hidden");
+      input.setAttribute("name", p);
+      input.setAttribute("value", params[p]);
+      form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
+  }
+
   return (
     <>
       {isLoggedIn ? (
@@ -99,7 +136,7 @@ function App() {
           </header>
           <div id="login-body">
             <div className="login">
-              <button onClick={() => login()} className="login-button">
+              <button onClick={() => oauthSignIn()} className="login-button">
                 Sign in to Lotion with{" "}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
